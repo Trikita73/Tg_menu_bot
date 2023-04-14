61ebcc6efd52798b44862f834165c52143d7792b
Trikita73 <diachenkoandrii7@gmail.com>
2023-04-14 14:22:32 +0300

    bot_pic

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import telebot
 
-bot=telebot.TeleBot('token')
+bot=telebot.TeleBot('6145733943:AAGMhy0O5bxlluF4tsC-9ktvTODiQpRsxUA')
 
 @bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, '<b>Hello!!!</b>', parse_mode='html')
 
+    
+def start_message(message):
+    bot.send_photo(message.chat.id, open('img/izy_i.jpg', 'rb'))
+    bot.send_message(message.chat.id, '<b>Hello!!!</b>', parse_mode='html')
+    
 
 bot.polling(none_stop=True)
