5fe59ad8e24cac93ef346e3868e76f23e8fedd7b
Trikita73 <diachenkoandrii7@gmail.com>
2023-04-27 22:59:42 +0300

    bot_tg

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import telebot
 from telebot import types
 
-bot=telebot.TeleBot('6145733943:AAGMhy0O5bxlluF4tsC-9ktvTODiQpRsxUA')
+bot=telebot.TeleBot('token')
 
 @bot.message_handler(commands=['start'])
 
     
 def start_message(message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton('Ганстер', callback_data=''))
+    markup.add(types.InlineKeyboardButton('Ганстер', callback_data='delete'))
     markup.add(types.InlineKeyboardButton('Перейти на сайт', url='https://trikita73.github.io/High-Wave/'))
     
 
-    bot.send_photo(message.chat.id, open('img/izy_i.jpg', 'rb'))
-    bot.send_message(message.chat.id, '<b>Hello!!!</b>', parse_mode='html', reply_markup=markup)
+    bot.send_photo(message.chat.id, open('img/izy_i_2.jpg', 'rb'))
+    bot.send_message(message.chat.id, '<b>Hello!!!</b><b>Hello!!!</b><b>Hello!!!</b>', parse_mode='html', reply_markup=markup)
 
 
 
